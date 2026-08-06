f4e4cae4058abf0651cb3a23fd0fe704791e94a8
AnimeDonghuo <165592897+AnimeDonghuo@users.noreply.github.com>
2026-08-06 14:14:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.getenv("API_ID", "0"))
+API_ID = int(os.getenv("API_ID", "26826540"))
 API_HASH = os.getenv("API_HASH", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 
